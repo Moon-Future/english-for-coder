65af0035f9ef001022b04adc15543741086e9f32
Leo Chan <236338364@qq.com>
2019-08-24 01:53:37 +0800

    data.json

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const Koa = require('koa')
 const app = new Koa()
-const session = require('koa-session') // session管理
 const cors = require('koa2-cors') // 解决跨域
 // const bodyParser = require('koa-bodyparser')  // 解析POST请求数据
 const koaBody = require('koa-body') // 可以上传文件
@@ -8,18 +7,6 @@ const static = require('koa-static')  // 关联静态文件
 const path = require('path')
 const router = require('./router')
 
-const CONFIG = {
-  key: 'koa:sess',
-  maxAge: 86400000,
-  autoCommit: true,
-  overwrite: true,
-  httpOnly: true,
-  signed: true,
-  rolling: false,
-  renew: false
-}
-app.keys = ['login secret']
-app.use(session(CONFIG, app));
 app.use(cors({}))
 // app.use(bodyParser())
 app.use(koaBody({
